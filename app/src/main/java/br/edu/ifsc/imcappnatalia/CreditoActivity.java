5c230dc91d462c2dece73307cc97387f71ac7842
alunaSabrina <binasantos1314@gmail.com>
2019-08-26 21:56:33 -0300

    Commit Exercicio 2

---
+++ app/src/main/java/br/edu/ifsc/imcappnatalia/CreditoActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsc.imcappnatalia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreditoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.creditos_layout);
+    }
+}
--- app/src/main/java/br/edu/ifsc/imcappnatalia/IMCRelativeLayout.java
+++ app/src/main/java/br/edu/ifsc/imcappnatalia/IMCRelativeLayout.java
@@ -2,7 +2,11 @@ package br.edu.ifsc.imcappnatalia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class IMCRelativeLayout extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class IMCRelativeLayout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imcrelative_layout);
+    }
+    public void onClickCalcularIMC(View view) {
+        EditText alturaEd = (EditText) findViewById(R.id.altura_ed);
+        EditText pesoEd = (EditText) findViewById(R.id.peso_ed);
+
+        double altura = Double.parseDouble(alturaEd.getText().toString());
+        double peso = Double.parseDouble(pesoEd.getText().toString());
+
+        TextView resultado = (TextView) findViewById(R.id.resulta_imc_tv);
+        double imc = peso/ (Math.pow(altura,2));
+        resultado.setText(String.format("Resultado: %.2f",(imc)));
+    }
+
+    public void onClickIniciarCreditosActivity(View view) {
+        Intent intent = new Intent(IMCRelativeLayout.this, CreditoActivity.class);
+        startActivity(intent);
     }
 }
