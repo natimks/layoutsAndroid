e29cd46ce46bbc99bde19c01bd6b76f26fc95b8c
alunaSabrina <binasantos1314@gmail.com>
2019-08-26 22:23:34 -0300

    Commit listener do botao calcular IMC

--- app/src/main/java/br/edu/ifsc/imcappnatalia/IMCRelativeLayout.java
+++ app/src/main/java/br/edu/ifsc/imcappnatalia/IMCRelativeLayout.java
@@ -5,27 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class IMCRelativeLayout extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imcrelative_layout);
-    }
-    public void onClickCalcularIMC(View view) {
-        EditText alturaEd = (EditText) findViewById(R.id.altura_ed);
-        EditText pesoEd = (EditText) findViewById(R.id.peso_ed);
 
-        double altura = Double.parseDouble(alturaEd.getText().toString());
-        double peso = Double.parseDouble(pesoEd.getText().toString());
+        button = findViewById(R.id.calcular_imc);
+        button.setOnClickListener(new View.OnClickListener(){
 
-        TextView resultado = (TextView) findViewById(R.id.resulta_imc_tv);
-        double imc = peso/ (Math.pow(altura,2));
-        resultado.setText(String.format("Resultado: %.2f",(imc)));
+            @Override
+            public void onClick(View view) {
+                EditText alturaEd = (EditText) findViewById(R.id.altura_ed);
+                EditText pesoEd = (EditText) findViewById(R.id.peso_ed);
+
+                double altura = Double.parseDouble(alturaEd.getText().toString());
+                double peso = Double.parseDouble(pesoEd.getText().toString());
+
+                TextView resultado = (TextView) findViewById(R.id.resulta_imc_tv);
+                double imc = peso/ (Math.pow(altura,2));
+                resultado.setText(String.format("Resultado: %.2f",(imc)));
+            }
+        });
     }
+//    public void onClickCalcularIMC(View view) {
+//        EditText alturaEd = (EditText) findViewById(R.id.altura_ed);
+//        EditText pesoEd = (EditText) findViewById(R.id.peso_ed);
+//
+//        double altura = Double.parseDouble(alturaEd.getText().toString());
+//        double peso = Double.parseDouble(pesoEd.getText().toString());
+//
+//        TextView resultado = (TextView) findViewById(R.id.resulta_imc_tv);
+//        double imc = peso/ (Math.pow(altura,2));
+//        resultado.setText(String.format("Resultado: %.2f",(imc)));
+//    }
 
     public void onClickIniciarCreditosActivity(View view) {
         Intent intent = new Intent(IMCRelativeLayout.this, CreditoActivity.class);
